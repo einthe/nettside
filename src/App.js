a99e53501f276fa7dbb67f6375e388c7536a0f5d
Einar Therkelsen <einthe@outlook.com>
2022-04-05 06:47:13 +0200

    write einars kule nettside

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Einars nettside
+          Einars kule nettside
         </p>
         <a
           className="App-link"
